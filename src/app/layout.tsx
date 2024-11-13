b2c962a36e4123a056025d65caebabe2e9fe28f2
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2024-11-13 13:47:52 +0100

    Set up typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,17 @@
 import type { Metadata } from 'next';
-import '../styles/index.scss';
+import '../styles/_globals.scss';
+import { Source_Serif_4, IBM_Plex_Mono } from 'next/font/google';
+
+const sourceSerif = Source_Serif_4({
+  subsets: ['latin'],
+  variable: '--font-source-serif',
+});
+
+const ibmPlexMono = IBM_Plex_Mono({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-ibm-plex-mono',
+});
 
 // TODO: Async fetch metadata
 export const metadata: Metadata = {
@@ -13,7 +25,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${sourceSerif.variable} ${ibmPlexMono.variable}`}
+    >
       <body>{children}</body>
     </html>
   );
