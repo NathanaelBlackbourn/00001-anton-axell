491426550a667aa937403463424e3721e646b0ce
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2024-11-13 10:07:44 +0100

    Fix: Import home page template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { client } from '@/sanity/lib/client';
-import HomePageTemplate from '@/templates/homePageTemplate';
+import HomePageTemplate from '@/templates/HomePageTemplate';
 import { groq } from 'next-sanity';
 
 export default async function Home() {
