d269e07a6aaa1db5deb9ce4653cdc23303776bcc
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2024-12-06 12:32:43 +0100

    Fix: Remove logs from useBreakpoint

--- src/lib/hooks/useBreakpoint.ts
+++ src/lib/hooks/useBreakpoint.ts
@@ -6,13 +6,10 @@ type Breakpoint = keyof typeof breakpoints;
 const useBreakpoint = (bp: Breakpoint) => {
   const [isBreakpoint, setIsBreakpoint] = useState(false);
 
-  console.log(breakpoints);
-
   useEffect(() => {
     const handleResize = () => {
       const width = window.innerWidth;
       const isBreakpoint = width >= parseInt(breakpoints[bp]);
-      console.log(breakpoints[bp], parseInt(breakpoints[bp]));
       setIsBreakpoint(isBreakpoint);
     };
 
@@ -21,10 +18,6 @@ const useBreakpoint = (bp: Breakpoint) => {
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, [bp]);
-
-  useEffect(() => {
-    console.log(isBreakpoint);
-  }, [isBreakpoint]);
 
   return isBreakpoint;
 };
