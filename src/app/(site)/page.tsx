940bf2f3a879ab623a9fd3082ee556f2d7dc3030
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2025-01-05 17:10:52 +0100

    Set up tag based revalidation

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,9 +1,12 @@
-import { client } from '@/sanity/lib/client';
+import { sanityFetch } from '@/sanity/lib/client';
 import { HOME_PAGE_QUERY } from '@/sanity/queries/pages/homePageQuery';
 import HomePageTemplate from '@/templates/HomePageTemplate';
 
 export default async function Home() {
-  const data = await client.fetch(HOME_PAGE_QUERY);
+  const data = await sanityFetch({
+    query: HOME_PAGE_QUERY,
+    tags: ['homePage', 'contact', 'about'],
+  });
 
   return data ? <HomePageTemplate {...data} /> : null;
 }
---
+++ src/app/api/revalidate-tag/route.ts
@@ -0,0 +1,43 @@
+import { revalidateTag } from 'next/cache';
+import { type NextRequest, NextResponse } from 'next/server';
+import { parseBody } from 'next-sanity/webhook';
+
+type WebhookPayload = {
+  _type: string;
+};
+
+export async function POST(req: NextRequest) {
+  console.log('Revalidation begin');
+
+  try {
+    if (!process.env.SANITY_REVALIDATE_SECRET) {
+      return new Response('Missing secret', { status: 401 });
+    }
+
+    const { isValidSignature, body } = await parseBody<WebhookPayload>(
+      req,
+      process.env.SANITY_REVALIDATE_SECRET
+    );
+
+    if (!isValidSignature) {
+      const message = 'Invalid signature';
+      return new Response(JSON.stringify({ message, isValidSignature }), {
+        status: 401,
+      });
+    } else if (!body?._type) {
+      const message = 'Bad Request';
+      return new Response(JSON.stringify({ message, body }), { status: 400 });
+    }
+
+    revalidateTag(body._type);
+
+    console.log('Revalidation completed for', body._type);
+
+    return NextResponse.json({ body });
+  } catch (err) {
+    if (err instanceof Error) {
+      return new Response(err.message, { status: 500 });
+    }
+    return new Response('Internal Server Error', { status: 500 });
+  }
+}
