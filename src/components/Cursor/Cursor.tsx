9f7e85391e080ab6658de203e593d79e5bb5dfcc
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2025-02-04 22:05:45 +0100

    WIP: Track size changes in hover elements

--- src/components/Cursor/Cursor.tsx
+++ src/components/Cursor/Cursor.tsx
@@ -1,18 +1,29 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
-import classes from './Cursor.module.scss';
-import { tweenCursorShape, tweenToPosition } from './animations';
 import { useCursor } from '@/lib/contexts/CursorContext';
+import { useEffect, useMemo, useRef } from 'react';
+import classes from './Cursor.module.scss';
+import { generateQuickTos, tweenToPosition, tweenToTarget } from './animations';
 
 const Cursor = () => {
   const cursorRef = useRef<HTMLDivElement>(null);
 
   const { hoverTarget } = useCursor();
 
+  const quickTos = useMemo(() => {
+    console.log('useMemo');
+    if (!cursorRef.current) return;
+    return generateQuickTos(cursorRef.current);
+  }, [cursorRef.current]);
+
+  useEffect(() => {
+    console.log(cursorRef.current);
+  }, [cursorRef]);
+
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
-      !hoverTarget && tweenToPosition(cursorRef, e.clientX, e.clientY);
+      if (hoverTarget) return;
+      quickTos && tweenToPosition(quickTos, e.clientX, e.clientY);
     };
 
     window.addEventListener('mousemove', handleMouseMove);
@@ -20,12 +31,35 @@ const Cursor = () => {
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
     };
-  }, [hoverTarget]);
+  }, [hoverTarget, quickTos]);
 
   useEffect(() => {
-    cursorRef.current &&
-      tweenCursorShape(cursorRef.current, hoverTarget && hoverTarget);
-  }, [hoverTarget]);
+    let observer: ResizeObserver | null = null;
+
+    const setObersver = () => {
+      observer = new ResizeObserver(() => {
+        quickTos && tweenToTarget(quickTos, hoverTarget!);
+      });
+
+      observer.observe(hoverTarget!);
+    };
+
+    const disconnectObserver = () => {
+      observer?.disconnect();
+      observer = null;
+    };
+
+    hoverTarget ? setObersver() : disconnectObserver();
+
+    return () => {
+      disconnectObserver();
+    };
+  }, [hoverTarget, quickTos]);
+
+  // useEffect(() => {
+  //   cursorRef.current &&
+  //     tweenCursorShape(cursorRef.current, hoverTarget && hoverTarget);
+  // }, [hoverTarget]);
 
   return <div ref={cursorRef} className={classes['cursor']} />;
 };
--- src/components/Cursor/animations.ts
+++ src/components/Cursor/animations.ts
@@ -1,41 +1,78 @@
-import { RefObject } from 'react';
 import { gsap } from '@/lib/gsap';
 import cursorStyles from './Cursor.module.scss';
 
+// export const tweenToPosition = (
+//   cursorRef: RefObject<HTMLDivElement>,
+//   x: number,
+//   y: number
+// ) => {
+//   gsap.to(cursorRef.current, {
+//     x: x - parseInt(cursorStyles.cursorSize) / 2,
+//     y: y - parseInt(cursorStyles.cursorSize) / 2,
+//     duration: 0.1,
+//   });
+// };
+
+// export const tweenCursorShape = (
+//   cursorRef: HTMLDivElement,
+//   hoverTarget: HTMLElement | null
+// ) => {
+//   const fitToTarget = () => {
+//     const box = hoverTarget!.getBoundingClientRect();
+//     gsap.to(cursorRef, {
+//       width: box.width,
+//       height: box.height,
+//       x: box.x,
+//       y: box.y,
+//       borderRadius: '2px',
+//       duration: 0.3,
+//     });
+//   };
+
+//   hoverTarget
+//     ? fitToTarget()
+//     : gsap.to(cursorRef, {
+//         width: 50,
+//         height: 50,
+//         borderRadius: '50%',
+//         duration: 0.3,
+//       });
+// };
+
+export const generateQuickTos = (cursorRef: HTMLDivElement) => {
+  console.log('generateQuickTos');
+  const duration = 0.01;
+  return {
+    x: gsap.quickTo(cursorRef, 'x', { duration }),
+    y: gsap.quickTo(cursorRef, 'y', { duration }),
+    width: gsap.quickTo(cursorRef, 'width', { duration }),
+    height: gsap.quickTo(cursorRef, 'height', { duration }),
+    borderRadius: gsap.quickTo(cursorRef, 'borderRadius', { duration }),
+  };
+};
+
+export const tweenToTarget = (
+  quickTos: ReturnType<typeof generateQuickTos>,
+  targetRef: HTMLElement
+) => {
+  console.log('tweenToTarget');
+  const box = targetRef.getBoundingClientRect();
+  quickTos.x(box.x);
+  quickTos.y(box.y);
+  quickTos.width(box.width);
+  quickTos.height(box.height);
+  quickTos.borderRadius(2);
+};
+
 export const tweenToPosition = (
-  cursorRef: RefObject<HTMLDivElement>,
+  quickTos: ReturnType<typeof generateQuickTos>,
   x: number,
   y: number
 ) => {
-  gsap.to(cursorRef.current, {
-    x: x - parseInt(cursorStyles.cursorSize) / 2,
-    y: y - parseInt(cursorStyles.cursorSize) / 2,
-    duration: 0.1,
-  });
-};
-
-export const tweenCursorShape = (
-  cursorRef: HTMLDivElement,
-  hoverTarget: HTMLElement | null
-) => {
-  const fitToTarget = () => {
-    const box = hoverTarget!.getBoundingClientRect();
-    gsap.to(cursorRef, {
-      width: box.width,
-      height: box.height,
-      x: box.x,
-      y: box.y,
-      borderRadius: '2px',
-      duration: 0.3,
-    });
-  };
-
-  hoverTarget
-    ? fitToTarget()
-    : gsap.to(cursorRef, {
-        width: 50,
-        height: 50,
-        borderRadius: '50%',
-        duration: 0.3,
-      });
+  console.log('tweebToPosition');
+  quickTos.x(x - parseInt(cursorStyles.cursorSize) / 2);
+  quickTos.y(y - parseInt(cursorStyles.cursorSize) / 2);
+  quickTos.width(parseInt(cursorStyles.cursorSize));
+  quickTos.height(parseInt(cursorStyles.cursorSize));
+  quickTos.borderRadius(50);
 };
